8c03992ca2ad7d3c7772c84e4b2f9c400fbb2f2e
Viktor <todosijevicviktor998@gmail.com>
2022-05-08 01:16:35 +0200

    Peak detection example

--- examples/spectrum.py
+++ examples/spectrum.py
@@ -1,2 +1,68 @@
+import numpy as np
+from scipy import signal
+from matplotlib import pyplot as plt
+
 if __name__ == '__main__':
-    pass
+    """
+    0. Do we bandpass filter before hand? Yeah, why not?
+    1. Downsample signal to get Fs to the 0-5 Hz (or less) range for better sampling of the spectrum
+    2. Window that signal to kill the side lobes
+    3. Calculate fft and abs(fft)
+    4. Find peaks
+    5. Sort peaks by height
+    6. Take the highest peaks (2 or how many you expect)
+    7. Read frequencies
+    """
+    Fs = 1000
+    t = np.arange(start=0, stop=3, step=1 / Fs)
+    f = [0.35, 1.4]
+    a = [1, 4]
+    x = 0
+    # TODO add noise. Or just look at the real signal
+    for i in range(len(f)):
+        x += a[i] * np.sin(2 * np.pi * f[i] * t)
+
+    flim = 3
+
+    plt.figure()
+    plt.plot(t, x)
+    plt.show()
+
+    # TODO what does range fft do? I don't think it does fft just in a small range
+    donwsample_factor = 100
+    x_downsampled = x[::donwsample_factor]
+    Nfft = 2 ** 8
+    window = np.hamming(len(x_downsampled))
+    # window = np.ones(len(x_downsampled))
+    fourier = np.fft.fft(x_downsampled * window, n=Nfft)
+    amp = np.abs(fourier * 2 / len(x_downsampled))
+    phase = np.angle(fourier)
+
+    faxis = np.fft.fftfreq(n=Nfft) * Fs / donwsample_factor
+
+    fig, ax = plt.subplots(2, 1)
+    ax[0].plot(faxis, amp)
+    ax[0].set_title('Amp')
+    ax[0].set_xlim(left=-flim, right=flim)
+    ax[1].plot(faxis, phase)
+    ax[1].set_title('Phase')
+    ax[1].set_xlim(left=-flim, right=flim)
+
+    fig.tight_layout()
+    fig.show()
+
+    right_lim = int(np.floor(Nfft / 2))
+    right_side_amp = amp[:right_lim]
+    peaks = signal.find_peaks(right_side_amp)
+
+    sorted_peak_indices = np.argsort(right_side_amp[peaks[0]])[::-1]
+    sorted_peaks = peaks[0][sorted_peak_indices]
+
+    highest_peaks = sorted_peaks[:2]
+
+    plt.figure()
+    plt.plot(faxis[:right_lim], right_side_amp)
+    plt.scatter(faxis[highest_peaks], right_side_amp[highest_peaks], color='r')
+    plt.show()
+
+    print('Peak frequencies: ', faxis[highest_peaks], ' Hz')
